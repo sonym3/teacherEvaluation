a874bad2da18b4bc1e1f603a1377a75e73c02243
sony <sonym3@gmail.com>
2019-11-28 00:55:58 -0500

    subject fetching

--- src/java/teacherevaluation/StudentSubject.java
+++ src/java/teacherevaluation/StudentSubject.java
@@ -5,26 +5,87 @@
  */
 package teacherevaluation;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Instant;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 /**
  *
  * @author HP
  */
+@Path("studentsubject")
+
 public class StudentSubject {
      @Context
     private UriInfo context;
-    @Path("singleStudent{id}")
-
+   
     @GET
+    @Path("getSubject&{pids}")
     @Produces("text/plain")
-    public String getText() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+public String getSubject(@PathParam("pids") int pids)
+       {
+   
+        Connection con=null;
+int flag=1;
+JSONObject mainJSONObject = new JSONObject();        
+        try{
+            DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+            con=DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "mad312team5", "anypw");
+            try (Statement stm = con.createStatement()) {
+                String sql="select sids,subject.SNAME from stusub \n" +
+                        "join student on student.PID=stusub.PIDS\n" +
+                        "join subject on stusub.SIDS=subject.SID\n" +
+                        "where student.PID="+pids;
+                ResultSet result = stm.executeQuery(sql);
+                
+                Instant instant=Instant.now();
+                long time=instant.getEpochSecond();
+                
+                
+                JSONArray array = new JSONArray();
+                JSONObject secondObject = new JSONObject();
+                
+                
+                mainJSONObject.accumulate("Status", "ERROR");
+                mainJSONObject.accumulate("Timestamp", time);
+                
+                
+                    while(result.next()){
+                       mainJSONObject.clear();
+                        mainJSONObject.accumulate("Status", "OK");
+                        mainJSONObject.accumulate("Timestamp", time);
+                        flag=1;
+                            secondObject.accumulate("subject_id", result.getInt(1));
+                            secondObject.accumulate("subject_name", result.getString(2));
+                            array.add(secondObject);
+                            secondObject.clear();
+                    }
+                    mainJSONObject.accumulate("Details of Subject", array);
+            }
+             con.close();
+
+           
+         } catch (SQLException ex) {
+            Logger.getLogger(Rating.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return mainJSONObject.toString();
+    
     }
+
+    
     
 }
